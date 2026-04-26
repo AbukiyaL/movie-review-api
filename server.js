122f17e689ff730a71c32a14177210b32d7d20b8
Abubeker-Adem-Code <allencollinsh1@gmail.com>
2026-04-27 02:02:11 +0300

    Implemented GET movies and 404 error handling

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const port = 3000;
+const filePath = path.join(__dirname, 'movies.json');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Movie API is running');
+   
+  if (req.method === 'GET' && req.url === '/movies') {
+    
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: "Error reading file" }));
+        return;
+      }
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(data);
+    });
+
+  } else {
+ 
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
 });
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
